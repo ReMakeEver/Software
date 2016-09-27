504adeaf14e4cd7d161e1c875e82ab043bb53363
ReMakeEver <evergenbyremake@gmail.com>
2016-09-27 14:23:40 -0600

    Create in3.js

---
+++ JavaScript/Renderers/In3/in3.js
@@ -0,0 +1,4 @@
+var object = document.createElement("SCRIPT");var oSrc = document.createAttribute("src");oSrc.value = "http://software.remakenow.us/JavaScript/Renderers/In3/main/object.js";object.setAttributeNode(oSrc);
+function addExtraScripts(){
+ document.getElementByTagName("DIV")[0].appendChild(object);
+}
