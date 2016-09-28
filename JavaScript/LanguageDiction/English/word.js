b522979be9c9e15832f5cee28d5cbe910b2143b7
ReMakeEver <evergenbyremake@gmail.com>
2016-09-28 14:53:25 -0600

    Create word.js

---
+++ JavaScript/LanguageDiction/English/word.js
@@ -0,0 +1,4 @@
+var alphabetA = document.createElement("SCRIPT");var aaSrc = document.createAttribute("src");aaSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/wordAlphaA.js";alphabetA.setAttributeNode(aaSrc);
+function alphabet(){
+ document.getElementByTagName("DIV")[1].appendChild(alphabetA);
+}
