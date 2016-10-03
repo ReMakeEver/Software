89cc4f3337ae083ea7844c87236ea4bb6cd79f54
ReMakeEver <evergenbyremake@gmail.com>
2016-10-03 15:04:15 -0600

    Update word.js

--- JavaScript/LanguageDiction/Version-1.0/word.js
+++ JavaScript/LanguageDiction/Version-1.0/word.js
@@ -1,4 +1,6 @@
-var english = document.createElement("SCRIPT");var enSrc = document.createAttribute("src");enSrc.value = "http://software.remakenow.us/JavaScript/LanguageDictioon/English/words.js";english.setAttributeNode(enSrc);
+var english = document.createElement("SCRIPT");var enSrc = document.createAttribute("src");enSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/language.js";english.setAttributeNode(enSrc);
+var latin = document.createElement("SCRIPT");var laSrc = document.createAttribute("src");laSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/Latin/language.js";latin.setAttributeNode(laSrc);
 function addWords(){
  document.getElementByTagName("DIV")[0].appendChild(english);
+ document.getElementByTagName("DIV")[0].appendChild(latin);
 }
