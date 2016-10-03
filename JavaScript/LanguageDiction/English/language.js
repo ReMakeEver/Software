958923faf731b076edf53621694f92c6e9893ff0
ReMakeEver <evergenbyremake@gmail.com>
2016-10-03 15:27:34 -0600

    Create language.js

---
+++ JavaScript/LanguageDiction/English/language.js
@@ -0,0 +1,17 @@
+var englishA = document.createElement("SCRIPT");var enaSrc = document.createElement("src");enaSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaA.js";englishA.setAttributeNode(enaSrc);
+var englishB = document.createElement("SCRIPT");var enbSrc = document.createElement("src");enbSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaB.js";englishB.setAttributeNode(enbSrc);
+var englishC = document.createElement("SCRIPT");var encSrc = document.createElement("src");encSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaC.js";englishC.setAttributeNode(encSrc);
+var englishD = document.createElement("SCRIPT");var endSrc = document.createElement("src");endSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaD.js";englishD.setAttributeNode(endSrc);
+var englishE = document.createElement("SCRIPT");var eneSrc = document.createElement("src");eneSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaE.js";englishE.setAttributeNode(eneSrc);
+var englishF = document.createElement("SCRIPT");var enfSrc = document.createElement("src");enfSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaF.js";englishF.setAttributeNode(enfSrc);
+var englishG = document.createElement("SCRIPT");var engSrc = document.createElement("src");engSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaG.js";englishG.setAttributeNode(engSrc);
+var englishH = document.createElement("SCRIPT");var enhSrc = document.createElement("src");enhSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaH.js";englishH.setAttributeNode(enhSrc);
+var englishI = document.createElement("SCRIPT");var eniSrc = document.createElement("src");eniSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaI.js";englishI.setAttributeNode(eniSrc);
+var englishJ = document.createElement("SCRIPT");var enjSrc = document.createElement("src");enjSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaJ.js";englishJ.setAttributeNode(enjSrc);
+var englishK = document.createElement("SCRIPT");var enkSrc = document.createElement("src");enkSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaK.js";englishK.setAttributeNode(enkSrc);
+var englishL = document.createElement("SCRIPT");var enlSrc = document.createElement("src");enlSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaL.js";englishL.setAttributeNode(enlSrc);
+var englishM = document.createElement("SCRIPT");var enmSrc = document.createElement("src");enmSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaM.js";englishM.setAttributeNode(enmSrc);
+var englishN = document.createElement("SCRIPT");var ennSrc = document.createElement("src");ennSrc.value = "http://software.remakenow.us/JavaScript/LanguageDiction/English/Alphabet/wordAlphaN.js";englishN.setAttributeNode(ennSrc);
+function addEnglish(){
+ document.getElementsByTagName("DIV")[1].appendChild(englishA);
+}
