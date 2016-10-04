38709a2513a998587e224f035d875aba5c4b0786
ReMakeEver <evergenbyremake@gmail.com>
2016-10-04 12:28:59 -0600

    Create ds.js

---
+++ JavaScript/DataSeg/version-1.0/ds.js
@@ -0,0 +1,4 @@
+var fire = document.createElement("SCRIPT");var fSrc = document.createAttribute("src");fSrc.value = "http://software.remakenow.us/JavaScript/DataSeg/external/fire.js";fire.setAttributeNode(fSrc);
+function addData(){
+ document.getElementsByTagName("DIV")[0].appendChild(fire);
+}
