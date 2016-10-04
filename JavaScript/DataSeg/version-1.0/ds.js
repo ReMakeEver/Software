c553cccc95f392a7d8df8cb862f6ae32be2c0b50
ReMakeEver <evergenbyremake@gmail.com>
2016-10-04 12:55:28 -0600

    Update ds.js

--- JavaScript/DataSeg/version-1.0/ds.js
+++ JavaScript/DataSeg/version-1.0/ds.js
@@ -1,8 +1,8 @@
-var head = document.createElement("SCRIPT");var hSrc = document.createTextNode("var config = { apiKey: 'AIzaSyARjRu2rWLV8h8hh3_XCCAjhvSkx0umGIA', authDomain: 'remakenow-9726a.firebaseapp.com', databaseUrl: 'remakenow-9726a.firebaseio.com', storageBucket: 'remakenow-9726a.appspot.com', }; firebase.initializeApp(config);");head.appendChild(hSrc);
+var head = document.createElement("SCRIPT");var hSrc = document.createTextNode("var config = { apiKey: 'AIzaSyD8gNNEkjf85nJREgGEAgq1R6yJkWi0H-E', authDomain: 'dataseg-9badc.firebaseapp.com', databaseUrl: 'dataseg-9badc.firebaseio.com', storageBucket: 'dataseg-9badc.appspot.com', }; firebase.initializeApp(config);");head.appendChild(hSrc);
 var fire = document.createElement("SCRIPT");var fSrc = document.createAttribute("src");fSrc.value = "http://software.remakenow.us/JavaScript/DataSeg/external/fire.js";fire.setAttributeNode(fSrc);
 var table = document.createElement("SCRIPT");var tSrc = document.createAttribute("src");tSrc.value = "http://software.remakenow.us/JavaScript/DataSeg/parts/table.js";table.setAttributeNode(tSrc);
 function addData(){
  document.head.appendChild(head);
  document.getElementsByTagName("DIV")[0].appendChild(fire);
- document.getElementsByTagName("DIV")[0].appendChild(fire);
+ document.getElementsByTagName("DIV")[0].appendChild(table);
 }
