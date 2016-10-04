304d2cb4c5d7c4a37099e0473bb1bc1a9e3bda64
ReMakeEver <evergenbyremake@gmail.com>
2016-10-04 12:40:23 -0600

    Update ds.js

--- JavaScript/DataSeg/version-1.0/ds.js
+++ JavaScript/DataSeg/version-1.0/ds.js
@@ -1,4 +1,6 @@
 var fire = document.createElement("SCRIPT");var fSrc = document.createAttribute("src");fSrc.value = "http://software.remakenow.us/JavaScript/DataSeg/external/fire.js";fire.setAttributeNode(fSrc);
+var head = document.createElement("SCRIPT");var hSrc = document.createTextNode("var config = { apiKey: 'AIzaSyARjRu2rWLV8h8hh3_XCCAjhvSkx0umGIA', authDomain: 'remakenow-9726a.firebaseapp.com', databaseUrl: 'remakenow-9726a.firebaseio.com', storageBucket: 'remakenow-9726a.appspot.com', }; firebase.initializeApp(config);");head.appendChild(hSrc);
 function addData(){
+ document.head.appendChild(head);
  document.getElementsByTagName("DIV")[0].appendChild(fire);
 }
