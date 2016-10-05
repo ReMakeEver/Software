556f2b25767c24ca68a3d51fe4050e906f568f62
ReMakeEver <evergenbyremake@gmail.com>
2016-10-05 11:02:23 -0600

    Update app.js

--- Applications/android/JavaScript/NoteMe/Version-1.0/app.js
+++ Applications/android/JavaScript/NoteMe/Version-1.0/app.js
@@ -1,3 +1,3 @@
 var lin = "Linear";
 var x = "FillX";var y = "FillY";var xy = "FillXY";
-var xH = "FillX,Horizontal";
+var xH = "FillX,Horizontal";var xV = "FillX,Vertical";var xL = "FillX,VLeft";var xC = "FillX,VCenter";var xR = "FillX,VRight";
