f77f5cde59ca5f5b0df4d877f92b92bafbce0801
ReMakeEver <evergenbyremake@gmail.com>
2016-10-07 13:47:32 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+var blockKey = "AIzaSyDfw-fOsNSfuJ9u7pwKekpNaMT40xXZFWk";
+var blockDom = "remadecoin.firebaseapp.com";
+var blockUrl = "https://remadecoin.firebaseio.com";
+var blockBuc = "remadecoin.appspot.com";
+var blockConfig = {
+ apiKey: blockKey,
+ authDomain: blockDom,
+ databaseUrl: blockUrl,
+ storageBucket: blockBuc
+}
