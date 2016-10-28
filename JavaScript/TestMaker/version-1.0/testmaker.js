41b2d7948d45662ecceea814eae95b879f2e9413
ReMakeEver <evergenbyremake@gmail.com>
2016-10-28 14:40:08 -0600

    Create testmaker.js

---
+++ JavaScript/TestMaker/version-1.0/testmaker.js
@@ -0,0 +1,25 @@
+var tName;
+function writeTest(name,questions){
+ tName = name.substring();
+ if(questions==1){
+  createQuestion1();
+ }
+ if(questions==2){
+  createQuestion1();createQuestion2();
+ }
+ if(questions==3){
+  createQuestion1();createQuestion2();createQuestion3();
+ }
+ if(questions==4){
+  createQuestion1();createQuestion2();createQuestion3();createQuestion4();
+ }
+ if(questions==5){
+  createQuestion1();createQuestion2();createQuestion3();createQuestion4();createQuestion5();
+ }
+ if(questions==6){
+  createQuestion1();createQuestion2();createQuestion3();createQuestion4();createQuestion5();createQuestion6();
+ }
+ if(questions==7){
+  createQuestion1();createQuestion2();createQuestion3();createQuestion4();createQuestion5();
+ }
+}
