43177d0b7aad30c236c8777248f88c962244db4f
ReMakeEver <evergenbyremake@gmail.com>
2016-12-02 14:32:01 -0700

    Update gamespace.js

--- JavaScript/GameSpace/Version-1.0/gamespace.js
+++ JavaScript/GameSpace/Version-1.0/gamespace.js
@@ -3,6 +3,8 @@ function newGame(){
 newGame.prototype = {
  scene: {
   value: function(){
+   var style = document.createElement("STYLE");var sTxt1 = "body{ margin: 0; }";var sTxt2 = "canvas{ width:100%;height:100%; }";var sTxt = document.createTextNode(sTxt1+sTxt2);style.appendChild(sTxt);
+   var can = document.createElement("CANVAS");
   },
   enumerable: true,
   configurable: true,
