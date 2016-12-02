5ba3f1121acc247b9ee6ee57daab6dcaf58fe196
ReMakeEver <evergenbyremake@gmail.com>
2016-12-02 14:04:08 -0700

    Delete dataseg.js

--- JavaScript/DataSeg/version-1.0/dataseg.js
+++
@@ -1,9 +0,0 @@
-function connect(key,databaseName){
- var config = {
-  apiKey: key.substring(),
-  authDomain: databaseName.substring+".firebaseapp.com",
-  databaseUrl: "https://"+databaseName.substring()+"firebaseio.com",
-  storageBucket: databaseName.substring()+".appspot.com"
- }
- firebase.initializeApp(config);
-}
