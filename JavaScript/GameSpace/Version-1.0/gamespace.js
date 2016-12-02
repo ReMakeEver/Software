f5aca79b00fae42e12b2c321b93420ee772c7fb5
ReMakeEver <evergenbyremake@gmail.com>
2016-12-02 14:13:00 -0700

    Create gamespace.js

---
+++ JavaScript/GameSpace/Version-1.0/gamespace.js
@@ -0,0 +1,13 @@
+function newGame(){
+}
+newGame.prototype = {
+ scene: {
+  value: function(){
+  },
+  enumerable: true,
+  configurable: true,
+  writable: true
+ }
+}
+newGame.prototype.constructor = NG;
+var ng = new NG();
