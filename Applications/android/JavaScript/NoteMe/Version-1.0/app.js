a25d66d6560aba9d18fa7fecd570dd90940a1dff
ReMakeEver <evergenbyremake@gmail.com>
2016-12-13 13:01:55 -0700

    Delete app.js

--- Applications/android/JavaScript/NoteMe/Version-1.0/app.js
+++
@@ -1,4 +0,0 @@
-var lin = "Linear";
-var x = "FillX";var y = "FillY";var xy = "FillXY";
-var xH = "FillX,Horizontal";var xV = "FillX,Vertical";var xL = "FillX,VLeft";var xC = "FillX,VCenter";var xR = "FillX,VRight";var xHL = "FillX,Horizontal,VLeft";var xHC = "FillX,Horizontal,VCenter";var xHR = "FillX,Horizontal,VRight";var xVL = "FillX,Vertical,VLeft";var xVC = "FillX,Vertical,VCenter";var xVR = "FillX,Vertical,VRight";
-var yH = "FillY,HorizontL";var yV ="FillY,Vertical";
