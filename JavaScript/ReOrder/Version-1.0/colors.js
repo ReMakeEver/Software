ec49f75d3bcc46add71bd9c88e59a8bf2dbf2ca7
ReMakeEver <evergenbyremake@gmail.com>
2017-01-20 14:02:06 -0700

    Create colors.js

---
+++ JavaScript/ReOrder/Version-1.0/colors.js
@@ -0,0 +1,18 @@
+function colorOrder(colors){
+ var array = colors.split(",");
+ var colorList = colors;
+ var len = array.length;
+ if(len==2){co2(colorList);}
+ if(len==3){co3(colorList);}
+ if(len==4){co4(colorList);}
+}
+function co2(colorList){
+ var array = colorList.split(",");
+ for(var i = 0;i<array.length;i++){
+  
+ }
+}
+function co3(){
+}
+function co4(){
+}
