a4e12f86cb90e8ca092bacb1d0d5db8f2fcb7ba7
ReMakeEver <evergenbyremake@gmail.com>
2017-02-01 12:49:50 -0700

    Create cureTicker.js

---
+++ JavaScript/PriceNow/cureTicker.js
@@ -0,0 +1,17 @@
+function getCUREPrice(id){
+ var xml;
+ if(window.XMLHttpRequest){
+  xml = new XMLHttpRequest();
+ }else{
+  xml = new ActiveXObject("Microsoft.XMLHTTP");
+ }
+ xml.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200){
+   var text = this.responseText;
+   var price = JSON.parse(text);
+   document.getElementById(id).innerHTML = price.ticker.price;
+  }
+ };
+ xml.open("GET", "https://api.cryptonator.com/api/ticker/cure-usd", true);
+ xml.send();
+}
