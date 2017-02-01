781adaa825a42642dfd5af6e5cf8dcef6bf56466
ReMakeEver <evergenbyremake@gmail.com>
2017-02-01 14:37:50 -0700

    Create crypt.js

---
+++ Encrypted/Version-1.0/crypt.js
@@ -0,0 +1,3 @@
+var a;var b;var c;var d;var e;var f;var g;var h;var i;var j;var k;var l;var m;var n;var o;var p;var q;var r;var s;var t;var u;var v;var w;var x;var y;var z;
+a = "b";b = "c";c = "d";d ="e";e = "f";f = "g";g = "h";h = "i";i = "j";j = "k";k = "l";l = "m";m = "n";n = "o";o = "p";p = "q";q = "r";r = "s";s = "t";t = "u";u = "v";v = "w";w = "x";x = "y";y = "z";z = "a";
+var A;var B;var C;var D;var E;var F;var G;var H;var I;var J;var K;var L;var M;var N;var O;var P;var Q;var R;var S;var T;var U;var V;var W;var X;var Y;var Z;
