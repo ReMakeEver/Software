47a12825df070bf83589e11093f12ec7befdda54
ReMakeEver <evergenbyremake@gmail.com>
2017-02-01 12:46:29 -0700

    Create ltcTicker.js

---
+++ JavaScript/PriceNow/ltcTicker.js
@@ -0,0 +1,17 @@
+function getLTCPrice(id){
+ var xml;
+ if(window.XMLHttpRequest){
+  xml = new XMLHttpRequest();
+ }else{
+  xml = new ActiveXObject("Microsoft.XMLHTTP");
+ }
+ xml.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200){
+   var text = this.responseText;
+   var price = JSON.parse(text);
+   document.getElementById(id).innerHTML = price.ticker.price;
+  }
+ }
+ xml.open("GET", "https://api.cryptonator.com/api/ticker/ltc-usd", true);
+ xml.send();
+}
