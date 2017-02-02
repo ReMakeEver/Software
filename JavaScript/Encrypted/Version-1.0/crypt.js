b111aef73272e475a8831ded45ab3b9efec968de
ReMakeEver <evergenbyremake@gmail.com>
2017-02-02 13:02:12 -0700

    Create crypt.js

---
+++ JavaScript/Encrypted/Version-1.0/crypt.js
@@ -0,0 +1,16 @@
+var a;var b;var c;var d;var e;var f;var g;var h;var i;var j;var k;var l;var m;var n;var o;var p;var q;var r;var s;var t;var u;var v;var w;var x;var y;var z;
+a = "b";b = "c";c = "d";d = "e";e = "f";f = "g";g = "h";h = "i";i = "j";j = "k";k = "l";l = "m";m = "n";n = "o";o = "p";p = "q";q = "r";r = "s";s = "t";t = "u";u = "v";v = "w";w = "x";x = "y";y = "z";z = "a";
+var A;var B;var C;var D;var E;var F;var G;var H;var I;var J;var K;var L;var M;var N;var O;var P;var Q;var R;var S;var T;var U;var V;var W;var X;var Y;var Z;
+A = "B";B = "C";C = "D";D = "E";E = "F";F = "G";G = "H";H = "I";I = "J";J = "K";K = "L";L = "M";M = "N";N = "O";O = "P";P = "Q";Q = "R";R = "S";S = "T";T = "U";U = "V";V = "W";W = "X";X = "Y";Y = "Z";Z = "A";
+var dataArray = new Array();
+function layer1(string){
+ var dat = string;
+ for(var i = 0;i<dat.length;i++){
+  if(dat[i]=="a"){add(a, 0);} 
+ }
+}
+function add(data, array){
+ var arr = array;
+ var dat = data;
+ dataArray[arr] += dat;
+}
