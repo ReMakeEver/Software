3e48fb2e290b3458f657d6f52b4d8c71f03cf59e
ReMakeEver <evergenbyremake@gmail.com>
2017-02-15 14:00:02 -0700

    Update scene.js

--- JavaScript/SceneJS/Version-1.0/scene.js
+++ JavaScript/SceneJS/Version-1.0/scene.js
@@ -1,7 +1,18 @@
 var SCENE = {
- "NewScene": function(w,h){
+ "NewScene": function(back,animated){
   var canvas = document.createElement("CANVAS");
-  document.body.appendChild(canvas);
+  var width = document.createAttribute("width");width.value = window.innerWidth;canvas.setAttributeNode(width);
+  var height = document.createAttribute("height");height.value = window.innerHeight;canvas.setAttributeNode(height);
+  if(back=="morning"){
+   if(animated=="yes"){
+   }else{
+    if(animated=="no"){
+     var context = canvas.getContext("2d");
+     var gradient = canvas.createLinearGradient(0,window.innerWidth,0,0);
+     gradient.addColorStop(0, "#FF5500");gradient.addColorStop(1, "#0088FF");
+     context.fillStyle = gradient;context.fillRect(0,0,window.innerWidth,window.innerHeight);
+    }
+   }
+  }
  }
 }
-var js = new SCENE();
