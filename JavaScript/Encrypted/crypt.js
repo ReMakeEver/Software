7c87ac0221bb8740d0cdcd62d0eaca8bb40d6489
ReMakeEver <evergenbyremake@gmail.com>
2017-02-22 13:48:00 -0700

    Delete crypt.js

--- JavaScript/Encrypted/crypt.js
+++
@@ -1,8 +0,0 @@
-var urlOne = 'https://remakeever.github.io/Software/JavaScript/Encrypted/Version-1.0/crypt.js';
-var urlTwo = 'https://remakeever.github.io/Software/JavaScript/Encrypted/Version-2.0/crypt.js';
-var urlThree = 'https://remakeever.github.io/Software/JavaScript/Encrypted/Version-3.0/crypt.js';
-var urlFour = 'https://remakeever.github.io/Software/JavaScript/Encrypted/Version-4.0/crypt.js';
-function addScript(dat){
- return "<script type='text/javascript' src="+dat+"></script>";
-}
-var verOne = new addScript(urlOne);
